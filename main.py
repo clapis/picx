426860745227a5d601da3fc28f35077b4f571f08
clapis <clapis@users.noreply.github.com>
2025-01-09 00:39:45 +0100

    update

--- main.py
+++ main.py
@@ -1,173 +1,151 @@
 import os
 import sys
-import time
-import magic
+import datetime
 import imagehash
+from pathlib import Path
 from PIL import Image, ExifTags
 
+class ImageInfo:
+    def __init__(self, path, hash, o_time, c_time, m_time, size, dimensions):
+        self.path = path
+        self.hash = hash
+        self.o_time = o_time
+        self.c_time = c_time
+        self.m_time = m_time
+        self.size = size
+        self.dimensions = dimensions
 
-def get_image_files(path):
-    """
-    Check path recursively for files. If any compatible file is found, it is
-    yielded with its full path.
-    :param path:
-    :return: yield absolute path
-    """
-    def is_image(file_name):
-        # List mime types fully supported by Pillow
-        full_supported_formats = ['gif', 'jp2', 'jpeg', 'pcx', 'png', 'tiff', 'x-ms-bmp',
-                                  'x-portable-pixmap', 'x-xbitmap']
-        try:
-            mime = magic.from_file(file_name, mime=True)
-            return mime.rsplit('/', 1)[1] in full_supported_formats
-        except IndexError:
-            return False
-
-    path = os.path.abspath(path)
-    for root, dirs, files in os.walk(path):
-        for file in files:
-            file = os.path.join(root, file)
-            if is_image(file):
-                yield file
-
-def get_file_info(file):
+def is_image(path):
     try:
-        print(f'info: {file}')
-        img = Image.open(file)
+        with Image.open(path) as img:
+            img.verify() 
+            return True
+    except Exception as e:
+        return False
 
-        file_size = get_file_size(file)
-        image_hash = get_image_hash(img)
-        image_size = get_image_size(img)
-        capture_time = get_capture_time(img)
+def get_image_info(path):
+    with Image.open(path) as img:
+        hash = get_image_hash(img)
+        size = get_file_size(path)
+        o_time = get_image_otime(img)
+        c_time = get_file_ctime(path)
+        m_time = get_file_mtime(path)
+        dimensions = get_image_dimensions(img)
 
-        # return file, image_hash, file_size, image_size, capture_time
-        return image_hash, capture_time
-    except OSError:
-        return None
+        return ImageInfo(path, hash, o_time, c_time, m_time, size, dimensions)
 
 def get_image_hash(img):
+    # slow, if just renaming files can skip this.
+    # return '' # <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
     hashes = []
-    # hash the image 4 times and rotate it by 90 degrees each time
-    for angle in [ 0, 90, 180, 270 ]:
-        if angle > 0:
-            turned_img = img.rotate(angle, expand=True)
-        else:
-            turned_img = img
+    for angle in [0, 90, 180, 270]:
+        turned_img = img.rotate(angle, expand=True) if angle > 0 else img
         hashes.append(str(imagehash.phash(turned_img)))
-
     return ''.join(sorted(hashes))
 
-def get_file_size(file_name):
-    try:
-        return os.path.getsize(file_name)
-    except FileNotFoundError:
-        return 0
+def get_file_size(path):
+    return os.path.getsize(path)
 
-def get_image_size(img):
-    return "{} x {}".format(*img.size)
+def get_image_dimensions(img):
+    return img.size
 
-def get_capture_time(img):
+def get_image_otime(img):
     try:
         exif = {
             ExifTags.TAGS[k]: v
             for k, v in img._getexif().items()
             if k in ExifTags.TAGS
         }
-        return exif["DateTimeOriginal"]
+
+        # format: 2024:10:27 14:39:19
+        return datetime.datetime.strptime(exif["DateTimeOriginal"], "%Y:%m:%d %H:%M:%S")
+
     except:
         return None
+    
+def get_file_ctime(path):
+    return datetime.datetime.fromtimestamp(os.stat(path).st_mtime)
 
-def move_file(file_name, trash):
-    if not os.path.exists(trash):
-        os.makedirs(trash)
-    try:
-        os.rename(file_name, trash + os.path.basename(file_name))
-        return True
-    except:
-        return False
+def get_file_mtime(path):
+    return datetime.datetime.fromtimestamp(os.stat(path).st_ctime)
 
+def progress(iterable, prefix = '', suffix = '', decimals = 1, length = 100, fill = '█', printEnd = "\r"):
+    total = len(iterable)
+    if total > 0: 
+        # Progress Bar Printing Function
+        def printProgressBar (iteration):
+            percent = ("{0:." + str(decimals) + "f}").format(100 * (iteration / float(total)))
+            filledLength = int(length * iteration // total)
+            bar = fill * filledLength + '-' * (length - filledLength)
+            print(f'\r{prefix} |{bar}| {percent}% {suffix}', end = printEnd)
+        # Initial Call
+        printProgressBar(0)
+        # Update Progress Bar
+        for i, item in enumerate(iterable):
+            yield item
+            printProgressBar(i + 1)
+        # Print New Line on Complete
+        print()
 
 #################################################
 
-def check_dates(path):
-    count = 0
-    total = 0
-    files = get_image_files(path)
+def load_images(dir):
+    files = [f.absolute() for f in Path(dir).iterdir() if f.is_file()]
+    return [get_image_info(file) for file in progress(files) if is_image(file)]
+
+def canonize_names(files):
+
     for file in files:
-        total += 1
-        image = Image.open(file)
-        captured = get_capture_time(image)
-        if captured == None:
-            count += 1
-            print(file)
-    print(count,total)
+        # print(file.path, file.o_time, file.c_time, file.m_time)
+        # continue
 
+        src = Path(file.path)
+        date = file.o_time or file.c_time
+        dest = src.with_name(f"{date.strftime("%Y-%m-%d %H.%M.%S")}{src.suffix}")
+        if file.path != dest: 
+            print(f'Renaming {file.path} -> {dest}')
+            # src.rename(dest)
 
+def find_duplicates(files):
+    # create dups destination
+    dups = os.path.join(dir, 'dups')
+    if not os.path.exists(dups):
+        os.makedirs(dups)
 
-def get_image_name(file, captured):
-
-    if captured == None:
-        return None
-
-    captured = captured.replace(':', '-', 2)
-    captured = captured.replace(':', '.', 2)
-    directory = os.path.dirname(file)
-    extension = os.path.splitext(file)[1].lower()
-
-    name = os.path.join(directory, captured + extension)
-
-    if name == file:
-        return name
-
-    i = 1
-    while os.path.exists(name):
-        name = os.path.join(directory, f"{captured}-{i}{extension}")
-        i += 1
-
-    return name 
-
-def group_duplicates(files):
-    files_info = {}
+    # group them by hash
+    hashmap = {}
     for file in files:
-        (hash, captured) = get_file_info(file)
-        if hash not in files_info:
-            files_info[hash] = []
-        files_info[hash].append((file, captured))
-    return files_info
+        if file.hash not in hashmap:
+            hashmap[file.hash] = []
+        hashmap[file.hash].append(file)
 
-def main(path):
-    trash = path + '/trash/'
-    files = get_image_files(path)
-    groups = group_duplicates(files)
+    # Move duplicates to dups folder
+    for hash, files in hashmap.items():
+        if len(files) > 1:
 
-    for hash, files in groups.items():
-        print(hash)
-        files.sort(key=lambda f: (f[1] or "", f[0]))
+            print(f'Found {len(files)} dups with hash {hash}:')
 
-        # Remove duplicates
-        for (dup_path, dup_cap) in files[:-1]:
-            print(f'trashing dup: {dup_path, dup_cap}')
-            move_file(dup_path, trash)
+            # sort them by date
+            files.sort(key=lambda f: (f.o_time or "", f.path))
 
-        # Rename file
-        (path, captured) = files[-1]
+            for file in files:
+                print(f'{file.path}: {file.o_time} {file.size} {file.dimensions}')
 
-        new_path = get_image_name(path, captured)
+            for file in files[:-1]:
+                files.remove(file)
+                dest = os.path.join(dups, os.path.basename(file.path))
+                print(f'moving: {file.path, dest}')
+                # os.rename(file.path, dest)
 
-        if new_path == None or new_path == path:
-            print(f'skipping: {path, captured}')
-        else:
-            print(f'renaming: {path} -> {new_path}')
-            os.rename(path, new_path)  
+def main(dir):
 
+    # get all images files in the directory
+    files = load_images(dir)
+
+    # find_duplicates(files)
+
+    canonize_names(files)
 
 
 if __name__ == '__main__':
-
-    # Check dates
-    # check_dates(sys.argv[1])
-
-    # Remove dups & rename
     main(sys.argv[1])
-
-
